eee04ba0c17f18538abcb9edae77e9580deafc8f
Keegan Ruebling <keegan.ruebling@gmail.com>
2017-11-03 14:12:15 -0700

    update firebase to include rentals

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,8 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-     return this.store.findAll('rental');
-   },
+    return Ember.RSVP.hash({
+      rentals: this.store.findAll('rental'),
+      reviews: this.store.findAll('review')
+    });
+  },
    actions: {
     saveRental3(params) {
       var newRental = this.store.createRecord('rental', params);
