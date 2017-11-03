182e3ffb625372f8e735a25c1da71fa56dc5a984
Keegan Ruebling <keegan.ruebling@gmail.com>
2017-11-03 14:53:40 -0700

    modify delete function to delete child elements before the parent

--- app/components/review-tile.js
+++ app/components/review-tile.js
@@ -1,4 +1,11 @@
-import Component from '@ember/component';
+import Ember from 'ember';
 
-export default Component.extend({
+export default Ember.Component.extend({
+  actions: {
+    delete(review) {
+      if (confirm('Are you sure you want to delete this review?')) {
+        this.sendAction('destroyReview', review);
+      }
+    }
+  }
 });
