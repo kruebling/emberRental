320e19fda94bc8574e31027fbe15ae3bccbc1938
Keegan Ruebling <keegan.ruebling@gmail.com>
2017-11-03 12:49:38 -0700

    update add form

--- app/components/update-rental.js
+++ app/components/update-rental.js
@@ -1,4 +1,22 @@
-import Component from '@ember/component';
+import Ember from 'ember';
 
-export default Component.extend({
+export default Ember.Component.extend({
+  isImageShowing: false,
+  updateRentalForm: false,
+  actions: {
+    imageShow() {
+      this.set('isImageShowing', true);
+    },
+    imageHide() {
+      this.set('isImageShowing', false);
+    },
+    update(rental, params) {
+      this.sendAction('update', rental, params);
+    },
+    delete(rental) {
+      if (confirm('Are you sure you want to delete this rental?')) {
+        this.sendAction('destroyRental', rental);
+      }
+    }
+  }
 });
